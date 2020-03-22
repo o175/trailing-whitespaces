3f51ace0089d3ad85f6610cf6b5cfab90ca54b8b
Wit <o175@ya.ru>
2020-03-22 20:10:06 +0300

    Add check for empty arguments

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ const tmp = require("tmp");
 const removeTrailingSpaces = require("remove-trailing-spaces");
 
 const patterns = process.argv.slice(2);
+if (!patterns.length) {
+  console.log("You should specify filenames or globs to process");
+  process.exit(0);
+}
 
 const files = fg.sync(patterns, { unique: true });
-console.log(process.argv);
-console.log(files);
 for (const i of files) {
+  console.log("Working on: " + i);
   const tmpobj = tmp.fileSync();
   const readInterface = readline.createInterface({
     input: fs.createReadStream(i),
     output: fs.createWriteStream(tmpobj.name, { flags: "a" }),
-    console: false
+    console: false,
   });
 
   readInterface
-    .on("line", function(line) {
+    .on("line", function (line) {
       this.output.write(removeTrailingSpaces(line) + "\n");
     })
-    .on("close", function(line) {
+    .on("close", function (line) {
       this.output.end();
       fs.copyFileSync(tmpobj.name, i);
       tmpobj.removeCallback();
